bbd8a5889a9ef18c87e6a90ef5c78646ea0fc301
Paul <androidaddict1992@gmail.com>
2022-12-30 08:45:36 -0600

    Added more endpoints
    Added more endpoints to program

--- axis-web.ps1
+++ axis-web.ps1
@@ -1,7 +1,15 @@
-# Set the FPS, Bandwidth, and Stream Profile values
+# Set the values for the cameras
 $fps = 7
-$bandwidth = 500
-$stream_profile = "Main"
+$zipstream = "Medium"
+$dynamicFPS = "on"
+$lowerLimit = 1
+$pFrames = 60
+$bitrateControl = "Maximum"
+$bitrate = 500
+$priority = "No Priority"
+$h264Profile = "Main"
+$dns1 = "169.88.8.8"
+$dns2 = "169.88.9.9"
 
 # Set the path to the Excel file
 $excel_file = "IP1.xlsx"
@@ -33,7 +41,7 @@ for ($i = $startRow; $i -le $endRow; $i++) {
         continue
     }
 
-    # Set the parameters for the H.264 stream profile
+    # Set the parameters for the 2nd stream H.264 stream profile
     $secresolution = "640x360"
     $secframeRate = 2
     $seccompression = 30
@@ -62,7 +70,6 @@ for ($i = $startRow; $i -le $endRow; $i++) {
     }
 
 
-
     # Set the FPS
     $setFpsResponse = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.FPS.FPS=$fps"
     # Check if the FPS was set successfully
@@ -72,8 +79,8 @@ for ($i = $startRow; $i -le $endRow; $i++) {
         Write-Host "Failed to set the FPS for camera $i"
     }
 
-    # Set the Bandwidth
-    $setBandwidthResponse = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.FPS.Bandwidth=$bandwidth"
+    # Set the Bitrate
+    $setBandwidthResponse = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.FPS.Bandwidth=$bitrate"
     # Check if the Bandwidth was set successfully
     if ($setBandwidthResponse.StatusDescription -match "200 OK") {
         Write-Host "Successfully set the Bandwidth for camera $i"
@@ -82,11 +89,93 @@ for ($i = $startRow; $i -le $endRow; $i++) {
     }
 
     # Set the Stream Profile
-    $setStreamProfileResponse = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.FPS.StreamProfile=$stream_profile"
+    $setStreamProfileResponse = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.FPS.StreamProfile=$h264Profile"
     # Check if the Stream Profile was set successfully
     if ($setStreamProfileResponse.StatusDescription -match "200 OK") {
         Write-Host "Successfully set the Stream Profile for camera $i"
     } else {
         Write-Host "Failed to set the Stream Profile for camera $i"
     }
+	
+	# Set the zipstream
+	$setZipStreamResponse = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.Zipstream=$zipstream"
+	# Check if zipstream was set successfully
+	if ($setZipStreamResponse.StatusDescription -match "200 OK") {
+		Write-Host "Successfully set the Zipstream for camera $i"
+	} else {
+		Write-Host "Failed to set the Zipstream for camera $i"
+	}
+	
+	# Enable Dynamic FPS
+	$setDynamicFps = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.DynamicFPS.Enabled=$dynamicFPS"
+	# Check if Dynamic FPS was set to enabled
+	if ($setDynamicFps.StatusDescription -match "200 OK") {
+		Write-Host "Successfully enabled Dynamic FPS for camera $i"
+	} else {
+		Write-Host "Failed to enable Dynamic FPS for camera $i"
+	}
+	
+	# Set the Dynamic Lower Limit FPS
+	$setDynamicLower = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.DynamicFPS.LowerLimit=$lowerLimit"
+	# Check if LowerLimit was set
+	if ($setDynamicLower.StatusDescription -match "200 OK") {
+		Write-Host "Successfully set the Dynamic Lower Limit for camera $1"
+	} else {
+		Write-Host "Failed to set Dynamic Lower Limit for camera $i"
+	}
+	
+	# Set the P-Frames
+	$setPframes = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.PFrames=$pFrames"
+	# Check if the P-Frames were set
+	if ($setPframes.StatusDescription -match "200 OK") {
+		Write-Host "Successfully set the PFrames for camera $i"
+	} else {
+		Write-Host "Failed to set the PFrames for camera $i"
+	}
+	
+	# Set the Bitrate Control
+	$setBitrateControl = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.BitrateConrol=$bitrateControl"
+	# Check if the bitrate control was set
+	if ($setBitrateControl.StatusDescription -match "200 OK") {
+		Write-Host "Successfully set Bitrate Control for camera $i"
+	} else {
+		Write-Host "Failed to set Bitrate Control for camera $i"
+	}
+	
+	# Set the priority
+	$setPriority = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.Priority=$priority"
+	# Check if the priority was set
+	if ($setPriority.StatusDescription -match "200 OK" {
+		Write-Host "Successfully set Priority on camera $i"
+	} else {
+		Write-Host "Failed to set Priority on camera $i"
+	}
+	
+	# Set the H.264 Profile
+	$setH264Profile = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&Video.H264Profile=$h264Profile"
+	# Check if the profile was set
+	if ($setH264Profile.StatusDescription -match "200 OK" {
+		Write-Host "Successfully set the H.264 Profile on camera $i"
+	} else {
+		Write-Host "Failed to set the H.264 Profile on camera $i"
+	}
+	
+	# Enable Motion Detection
+	$setMotionDetection = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&MothingDetectionApp=enabled"
+	# Check if Motion Detection was enabled
+	if ($setMotionDetection.StatusDescription -match "200 OK" {
+		Write-Host "Successfully Enabled Motion Detection for camera $i"
+	} else {
+		Write-Host "Failed to enable Motion Detection for camera $i"
+	}
+	
+	# Set DNS1 and 2
+	$setDnsNetwork = Invoke-WebRequest -Uri "http://$cameraIp/axis-cgi/admin/param.cgi?action=update&PrimaryDNS=$dns1&SecondaryDNS=$dns2"
+	# Check if the DNS was set correctly
+	if ($setDnsNetwork.StatusDescription -match "200 OK" {
+		Write-Host "Successfully set DNS1 and DNS2 for camera $i"
+	} else {
+		Write-Host "Failed to set DNS1 and DNS2 for camera $i"
+	}
+	
 }
